c2dff3892cf93108d1316c01213010ae63e1a716
vincent <ododovincent54@gmail.com>
2019-05-16 17:20:48 +0300

    everything

---
+++ animations.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $(".lionk").click(function() {
+      $(".lionp").show();
+    });
+    $(".lionk").click(function() {
+      $(".videolion").show();
+    });
+    $(".lionp").click(function(){
+      $(".lionp").hide();
+    });
+    $(".lionp").click(function() {
+      $(".videolion").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".spidermanin").click(function() {
+      $(".spiderp").show();
+    });
+    $(".spidermanin").click(function() {
+      $(".videospider").show();
+    });
+    $(".spiderp").click(function(){
+      $(".spiderp").hide();
+    });
+    $(".spiderp").click(function() {
+      $(".videospider").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".HOWTOTRAIN").click(function() {
+      $(".trainp").show();
+    });
+    $(".HOWTOTRAIN").click(function() {
+      $(".videotrain").show();
+    });
+    $(".trainp").click(function(){
+      $(".trainp").hide();
+    });
+    $(".trainp").click(function() {
+      $(".videotrain").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".SMALLFOOTA").click(function() {
+      $(".smallfootp").show();
+    });
+    $(".SMALLFOOTA").click(function() {
+      $(".videofoot").show();
+    });
+    $(".smallfootp").click(function(){
+      $(".smallfootp").hide();
+    });
+    $(".smallfootp").click(function() {
+      $(".videofoot").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".SPIESIN").click(function() {
+      $(".spiesp").show();
+    });
+    $(".SPIESIN").click(function() {
+      $(".videospies").show();
+    });
+    $(".spiesp").click(function(){
+      $(".spiesp").hide();
+    });
+    $(".spiesp").click(function() {
+      $(".videospies").hide();
+    });
+  });
---
+++ series.js
@@ -0,0 +1,27 @@
+$("document").ready(function(){
+    $(".three").click(function(){
+      $(".movie1").slideToggle();
+      $(".three").slideToggle();
+    });
+    $(".crown").click(function(){
+      $(".movie2").slideToggle();
+      $(".crown").slideToggle();
+    });
+  
+    $(".lucifer").click(function(){
+      $(".movie3").slideToggle();
+      $(".lucifer").slideToggle();
+    });
+    $(".ozak").click(function(){
+      $(".movie4").slideToggle();
+      $(".ozak").slideToggle();
+    });
+  
+    $(".btn").click(function(){
+      $(".down").show();
+    });
+    $(".down").click(function(){
+      $(".down").hide();
+    });
+  })
+  
